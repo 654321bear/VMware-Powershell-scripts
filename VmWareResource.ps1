5b214974655d9dea5f6e0fb88b53011e4d6127d6
654321bear <49491804+654321bear@users.noreply.github.com>
2020-04-24 17:20:45 -0500

    Update VmWareResource.ps1

--- VmWareResource.ps1
+++ VmWareResource.ps1
@@ -5,8 +5,6 @@
 Connect-VIServer -Server PROD-vcenter 
 #Connect-VIServer -Server NV-vcenter
 
-#Get-VM | Where { $_.VMHost -eq "prod-vmware4.meritenergy.com" -or $_.VMHost -eq "prod-vmware5.meritenergy.com" -and $_.PowerState -eq "PoweredOn"}| 
-
 Get-VM | Where { $_.name -eq "vdev-dwd-aci" -or $_.name -eq "vdev-MD1-ACI"  -or $_.name -eq "vprod-dwp-aci"  -or $_.name -eq "vprod-MP1-ACI"  -or $_.name -eq "vprod-MP1-AS1"  -or $_.name -eq "vprod-MP1-AS2"  -or $_.name -eq "vprod-SMP-ACI" -or $_.name -eq "vqa-MQ1-ACI" -or $_.name -eq "vqa-MQ1-AS1" -or $_.name -eq "vqa-MQ1-AS2" }| 
 Select Name, VMHost, NumCpu, MemoryMB,
  @{N="Cpu.UsageMhz.Average";E={[Math]::Round((($_ |Get-Stat -Stat cpu.usagemhz.average -Start (Get-Date).AddHours(-24)-IntervalMins 5 -MaxSamples (84) |Measure-Object Value -Average).Average),2)}},
