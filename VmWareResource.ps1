4ecee1ad46e04294ad31d784f077bd62cdbdc928
654321bear <49491804+654321bear@users.noreply.github.com>
2021-10-25 15:30:42 -0500

    Add files via upload

--- VmWareResource.ps1
+++ VmWareResource.ps1
@@ -2,13 +2,15 @@
 #Set-PowerCLIConfiguration -DefaultVIServerMode multiple -Confirm:$false
 
 #Connect to all Vmware Environments
-Connect-VIServer -Server PROD-vcenter 
-#Connect-VIServer -Server NV-vcenter
+Connect-VIServer -Server vcenter 
 
-Get-VM | Where { $_.name -eq "vdev-dwd-aci" -or $_.name -eq "vdev-MD1-ACI"  -or $_.name -eq "vprod-dwp-aci"  -or $_.name -eq "vprod-MP1-ACI"  -or $_.name -eq "vprod-MP1-AS1"  -or $_.name -eq "vprod-MP1-AS2"  -or $_.name -eq "vprod-SMP-ACI" -or $_.name -eq "vqa-MQ1-ACI" -or $_.name -eq "vqa-MQ1-AS1" -or $_.name -eq "vqa-MQ1-AS2" }| 
+
+#Get-VM | Where { $_.VMHost -eq "<VM_HOSTNAME>" -or $_.VMHost -eq "<VM_HOSTNAME>" -and $_.PowerState -eq "PoweredOn"}| 
+
+Get-VM | Where { $_.name -eq "<COMPUTER>" -or $_.name -eq "<COMPUTER>"  -or $_.name -eq "<COMPUTER>"  -or $_.name -eq "<COMPUTER>"  -or $_.name -eq "<COMPUTER>"  -or $_.name -eq "<COMPUTER>"  -or $_.name -eq "<COMPUTER>" -or $_.name -eq "<COMPUTER>" -or $_.name -eq "<COMPUTER>" -or $_.name -eq "<COMPUTER>" }| 
 Select Name, VMHost, NumCpu, MemoryMB,
  @{N="Cpu.UsageMhz.Average";E={[Math]::Round((($_ |Get-Stat -Stat cpu.usagemhz.average -Start (Get-Date).AddHours(-24)-IntervalMins 5 -MaxSamples (84) |Measure-Object Value -Average).Average),2)}},
  @{N="Mem.Usage.Average";E={[Math]::Round((($_ |Get-Stat -Stat mem.usage.average -Start (Get-Date).AddHours(-24)-IntervalMins 5 -MaxSamples (84) |Measure-Object Value -Average).Average),2)}} `
  | Export-Csv c:\Temp\1Day-Stats.csv
 
- Disconnect-VIServer PROD-vcenter  -confirm:$false
+ Disconnect-VIServer vcenter  -confirm:$false
